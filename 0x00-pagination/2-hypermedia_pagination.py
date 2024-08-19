62058a60f6a9afcb26bd31460505c50df1919979
khaledkhedr18 <khaledkhedr18@gmail.com>
2024-08-20 01:06:55 +0300

    task 2

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -70,8 +70,9 @@ class Server:
         """
         data = self.get_page(page, page_size)
         total_pages = math.ceil(len(self.__dataset) / page_size)
-        next_page = page + 1 if page < len(self.__dataset) else None
-        prev_page = page - 1 if page > 1 else None
+        start, end = index_range(page, page_size)
+        next_page = page + 1 if end < len(self.__dataset) else None
+        prev_page = page - 1 if start > 0 else None
         myDic = {
             'page_size': len(data),
             'page': page,
