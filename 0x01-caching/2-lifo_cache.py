491199626a201a96e408fe50e5592c6477691a7d
khaledkhedr18 <khaledkhedr18@gmail.com>
2024-08-21 20:56:18 +0300

    task 2

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -7,11 +7,22 @@ from base_caching import BaseCaching
 
 class LIFOCache(BaseCaching):
     def __init__(self):
+        '''Initialize.'''
         super().__init__()
         self.cache_data = {}
         self.keys_order = []
 
     def put(self, key, item):
+        """
+        Adds an item to the cache.
+
+        Parameters:
+            key (Any): The key to associate with the item.
+            item (Any): The item to store in the cache.
+
+        Returns:
+            None
+        """
         if key is None or item is None:
             return
         if key in self.cache_data:
@@ -21,9 +32,18 @@ class LIFOCache(BaseCaching):
         self.keys_order.append(key)
 
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            last_key = self.keys_order.pop()
+            last_key = self.keys_order.pop(BaseCaching.MAX_ITEMS - 1)
             del self.cache_data[last_key]
             print(f"DISCARD: {last_key}")
 
     def get(self, key):
+        """
+        Retrieves a value from the cache by its key.
+
+        Args:
+            key (Any): The key associated with the desired value.
+
+        Returns:
+            Any: The cached value associated with the given key.
+        """
         return self.cache_data[key]
