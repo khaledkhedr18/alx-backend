9fd3591923b65277d24cce66e8ee1563f54d3138
khaledkhedr18 <khaledkhedr18@gmail.com>
2024-08-28 01:11:57 +0300

    task 1

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+'''Task 0's Module'''
+from flask import Flask, request, render_template
+from flask_babel import Babel  # type: ignore
+
+
+class Config:
+    '''Simple Config Class'''
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@app.route('/')
+def get_index() -> str:
+    """
+    Defines a route for the root URL ('/') of the web application.
+
+    Returns:
+        The rendered HTML template located at '1-index.html'.
+    """
+    return render_template('1-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(['LANGUAGES'])
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
